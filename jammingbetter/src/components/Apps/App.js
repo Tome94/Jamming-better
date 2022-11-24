7b5c759f7139f970c1a641f3e701477e42772c34
Tome94 <tommy_yang621@hotmail.com>
2022-11-24 19:16:28 +0000

    hook up search bar to spotify search pt1

--- jammingbetter/src/components/Apps/App.js
+++ jammingbetter/src/components/Apps/App.js
@@ -38,6 +38,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
     this.savePlaylist = this.savePlaylist.bind(this)
+    this.search = this.search.bind(this)
   }
 
   addTrack(track) {
@@ -62,6 +63,9 @@ class App extends React.Component {
     // eslint-disable-next-line
     const trackUris = this.state.playlistTracks.map(track=>track.uri)
   }
+  search(term){
+    console.log(term)
+  }
   render() {
     return (
       <div> 
@@ -69,7 +73,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch ={this.search}/>
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
